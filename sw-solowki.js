1aabf331d6e256eaec4e77bd08a70d15fb0c2048
WujekSzpon <tomaszczerniuk@gmail.com>
2026-08-23 15:06:48 +0200

    Ulepsz filtrowanie nowości i podgląd kart

--- sw-solowki.js
+++ sw-solowki.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'najlepsze-polskie-solowki-v2';
+const CACHE_NAME = 'najlepsze-polskie-solowki-v3';
 const APP_SHELL = [
   './',
   './manifest-solowki.webmanifest',
@@ -42,8 +42,10 @@ self.addEventListener('fetch', event => {
 
   event.respondWith(
     caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
-      const copy = response.clone();
-      caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+      if (response.ok) {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+      }
       return response;
     }))
   );
